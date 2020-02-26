024a87aa3cd2472f3045c35d2ab67bdec9cc761b
filipefilardi <filipefilardi@gmail.com>
2020-02-26 12:46:04 -0300

    Add first version of client

---
+++ src/app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def home():
+    df_ratings = pd.read_csv('../data/ratings.csv')
+
+    genre_cols = [col.replace('genre_', '') for col in df_ratings.columns if col.startswith('genre_')]
+
+    genre = request.args.get('genre', None)
+    sort_by = request.args.get('sort', None)
+
+    if sort_by:
+        if sort_by == 'rate':
+            df_ratings = df_ratings.sort_values(['rate', 'votes'], ascending=False)
+        if sort_by == 'votes':
+            df_ratings = df_ratings.sort_values(['votes', 'rate'], ascending=False)
+
+    if genre:
+        df_ratings = df_ratings[df_ratings[f'genre_{genre}'] == 1]
+
+    return render_template("home.html", df_animes=df_ratings.head(100), genres=genre_cols)
+
+if __name__ == '__main__':
+    flask_options = dict(
+        debug=True,
+    )
+    app.run(**flask_options)
